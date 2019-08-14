4c21642a20d3cf35a105e8d3cc1d960611c4ced1
Vivek Malhan <36576957+docvvk@users.noreply.github.com>
2019-08-14 11:34:06 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,57 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import { Map, TileLayer } from 'react-leaflet';
+import annyang from 'annyang';
 import './App.css';
+class App extends Component {
+  state = {
+    center: [41.878099, -87.648116],
+    zoom: 12,
+  };
+  zoomIn = () => {
+    this.setState({
+      zoom: this.state.zoom + 1
+        })
+  }
+  zoomOut = () => {
+    this.setState({
+      zoom: this.state.zoom -1
+    })
+  }
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  componentDidMount() {
+    annyang.addCommands({
+      'in': this.zoomIn,
+      'out': this.zoomOut
+    });
+    annyang.start();
+  }
+
+
+  updateViewport = (viewport) => {
+    this.setState({
+      center: viewport.center,
+      zoom: viewport.zoom,
+    });
+  };
+  render() {
+    const {
+      center,
+      zoom,
+    } = this.state;
+    return (
+      <div className="App">
+        <Map
+          style={{height: '100%', width: '100%'}}
+          center={center}
+          zoom={zoom}
+          onViewportChange={this.updateViewport}>
+          <TileLayer
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            attribution="&copy; <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
+          />
+        </Map>
+      </div>
+    )
+  }
 }
-
 export default App;
